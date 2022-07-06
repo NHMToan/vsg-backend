6a497c674688e7ff55b6957f68edcd9c880d0fd8
Toan Nguyen <toan.nguyen@servicenode.se>
2022-07-06 09:26:50 +0700

    Up

--- src/resolvers/greeting copy.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from "type-graphql";
-
-@Resolver()
-export class GreetingResolver {
-  @Query((_return) => String)
-  async hello(): Promise<string> {
-    return "Hello World";
-  }
-}
--- src/resolvers/greeting.ts
+++ src/resolvers/greeting.ts
@@ -1,14 +1,9 @@
-import { Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
-import { User } from "../entities/User";
-import { checkAuth } from "../middleware/checkAuth";
-import { Context } from "../types/Context";
+import { Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class GreetingResolver {
   @Query((_return) => String)
-  @UseMiddleware(checkAuth)
-  async hello(@Ctx() { user }: Context): Promise<string> {
-    const existingUser = await User.findOne(user.userId);
-    return `Hello ${existingUser ? existingUser.lastName : "World"}`;
+  async hello(): Promise<string> {
+    return "Hello World";
   }
 }
